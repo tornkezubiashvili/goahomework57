a842c7aec8915a194cabbadcc619e9ef4b6f1abe
tornikezubiashvili <gtornike64@gmail.com>
2025-09-27 19:23:39 +0400

    commit

--- day101/classwork/cw05.js
+++ day101/classwork/cw05.js
@@ -2,7 +2,7 @@ let num = prompt("Enter your name; ")
 
 
 for(let i = 0; i<=num;i++){
-    if(i % 2 == 0){
+    if(i % 2 != 0){
         console.log(i)
     }
 }
---
+++ day101/classwork/cw06.js
@@ -0,0 +1,11 @@
+let num = prompt("Enter your name; ")
+
+
+for(let i = 0; i<=num;i++){
+    if(i % 2 != 0){
+        console.log(i +"ლუწია")
+    }
+    else{
+        console.log(i +"კენტია")
+    }
+}
