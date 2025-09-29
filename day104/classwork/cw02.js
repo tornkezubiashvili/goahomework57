99c700162b46c10db8bf20a11bca778071b486a8
tornikezubiashvili <gtornike64@gmail.com>
2025-09-29 22:49:59 +0400

    commit

---
+++ day104/classwork/cw02.js
@@ -0,0 +1,9 @@
+function nunc(arr) {
+    
+    console.log(arr.length)
+    console.log(arr.slice(3,8))
+    console.log(arr.splice(4,3))
+}
+
+
+nunc(["tornike", "nika", "goga", "luka", "gori", "tbilisi", "Goa", "irakli", "soso", "gio"])
---
+++ day104/classwork/cw03.js
@@ -0,0 +1,6 @@
+function list(a,b){
+    let both = a.concat(b)
+    console.log(both)
+}
+
+list([1,564,756],[23,456,234])
