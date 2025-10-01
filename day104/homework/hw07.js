6c060ff50cfda64397ccc4dcfd0208d1aa481dc0
tornikezubiashvili <gtornike64@gmail.com>
2025-10-01 23:36:48 +0400

    commit

---
+++ day104/homework/hw07.js
@@ -0,0 +1,8 @@
+//ფუნქციამ მიიღოს სია და ქალაქის სახელი. დაამატოს ეს ქალაქი სიის დასაწყისში და დააბრუნოს სია.
+
+function list(arr){
+    arr.unshift("gori")
+    console.log(arr)
+}
+
+list(["tbilisi","qobuleti","zugdidi"])
