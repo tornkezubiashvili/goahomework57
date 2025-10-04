5cfad4894726a395106bc37649a5998e1832aa8b
tornikezubiashvili <gtornike64@gmail.com>
2025-10-04 14:33:30 +0400

    commit

---
+++ day105/homework/hw02.js
@@ -0,0 +1,11 @@
+//შექმენი ფუნქცია, რომელიც იღებს არგუმენტად სტუდენტების სახელების
+//სიას და სტუდენტის სახელს და ამ სიას ბოლოში ამატებს სტუდენტის სახელს.
+
+
+function list(arr){
+    namelist = ["lado","nodari"]
+    arr.push(namelist)
+    console.log(arr)
+}
+
+list(["bondo","luka"])
