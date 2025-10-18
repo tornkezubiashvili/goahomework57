57d34cd2f6b8ec949357ce0bd7f761f10cb4a568
tornikezubiashvili <gtornike64@gmail.com>
2025-10-18 15:32:19 +0400

    commit

--- day058/homework/hw02.py
+++ day058/homework/hw02.py
@@ -8,32 +8,39 @@ d = ["AMG","BMW","AUDI"]
 print(a.upper())
 print(b.upper())
 print(c.upper())
+print("1------------------------------")
 
 print(a.lower())
 print(b.lower())
 print(c.lower())
+print("2------------------------------")
 
 print(a.capitalize())
 print(b.capitalize())
 print(c.capitalize())
+print("3------------------------------")
 
 print(a.find("a"))
 print(b.find("G"))
 print(c.find("B"))
+print("4------------------------------")
 
 print(len(a))
 print(len(b))
 print(len(c))
+print("5------------------------------")
 
 d.append(1)
 d.append(10)
 d.append(24)
+print("6------------------------------")
 
 print(d)
 
 d.insert(1,12)
 d.insert(2,56)
 d.insert(5,2)
+print("7------------------------------")
  
 print(d)
 
@@ -42,3 +49,4 @@ d.pop(2)
 d.pop(3)
 
 print(d)
+print("8------------------------------")
