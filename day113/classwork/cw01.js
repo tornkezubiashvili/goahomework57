fbaad102fd935d8a87251dcd7e023642b14bf9db
tornikezubiashvili <gtornike64@gmail.com>
2025-11-03 21:37:42 +0400

    commit

---
+++ day113/classwork/cw01.js
@@ -0,0 +1,19 @@
+// 1)შექმენი ობიექტი ტქვენს შესახებ,უნდა გქონდეთ ინფორმაცია --> სახელი , გვარი ,
+//  ასაკი , მისამართი, სიმაღლე , სასწავლებელი ადგილი,ამის შემდეგ კონსოლში გამოიტანეთ თითოეული value
+
+
+let object1 = {
+    name : "tornike",
+    surname : "zubiashvili",
+    age : 17,
+    addres: "Gori",
+    height : 180 ,
+    school : "GOA"
+}
+console.log(object1)
+console.log(object1.name)
+console.log(object1.surname)
+console.log(object1.age)
+console.log(object1.addres)
+console.log(object1.height)
+console.log(object1.school)
