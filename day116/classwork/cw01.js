b9aaf3f6abde4fd35e90d516ce17c3ef647b02a4
tornikezubiashvili <gtornike64@gmail.com>
2025-11-14 21:41:07 +0400

    commit

---
+++ day116/classwork/cw01.js
@@ -0,0 +1,9 @@
+// 1)Math.random() ის გამოუენებით დააგენერირეთ რიცხვები 0 დან 1 მდე
+// 2))Math.random() ის გამოუენებით დააგენერირეთ რიცხვები 0 დან 20 მდე
+// 3)Math.random() ის გამოუენებით დააგენერირეთ რიცხვები 1 დან 20 ჩათვლით
+
+console.log("დავალება1",  Math.random())
+
+console.log("დავალება2",Math.floor(Math.random()* 20))
+
+console.log("დავალება3",Math.floor(Math.random()* 20) +1)
