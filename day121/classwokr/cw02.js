755b6e77a1b2adcfed87bb96c85df7d77479e04e
tornikezubiashvili <gtornike64@gmail.com>
2025-12-01 22:41:06 +0400

    commit

---
+++ day121/classwokr/cw02.js
@@ -0,0 +1,16 @@
+let form = document.getElementById('form')
+
+form.addEventListener('submit',function(event){
+    event.preventDefault()
+    console.log(event.target.name.value)
+    console.log(event.target.suenamename.value)
+    console.log(event.target.age.value)
+
+    let liName = document.getElementById('liName')
+    let liSurname = document.getElementById('liSuname')
+    let liAge = document.getElementById('liAge')
+
+    liName.textContent = event.target.name.value
+    liSurname.textContent = event.target.suenamename.value
+    liAge.textContent = event.target.age.value
+})
