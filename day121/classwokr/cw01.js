f84f6232b09f10eb9289429fb216d84ab5d82182
tornikezubiashvili <gtornike64@gmail.com>
2025-12-01 21:57:02 +0400

    commit

---
+++ day121/classwokr/cw01.js
@@ -0,0 +1,11 @@
+//1) შექმენით ფორმა სადაც იქნება რამდენიმე ინფუთი, სადაც შეიყვანთ სახელს, გვარს, ასაკს, მეილს, შემდეგ კი js ში როდესაც ფორმა დასაბმითდება კონსოლში დაბეჭდეთ ის მონაცემები რაც მომხმარებელმა ინფუთებში შეიყვანა
+
+let form = document.getElementById('form')
+
+form.addEventListener('submit',function(event){
+    event.preventDefault()
+
+    console.log(event.target.name.value)
+    console.log(event.target.suenamename.value)
+    console.log(event.target.age.value)
+})
