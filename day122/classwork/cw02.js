34b9bfd26ae2c9345e3d28d1ae0502d4a898c860
tornikezubiashvili <gtornike64@gmail.com>
2025-12-05 23:00:32 +0400

    commit

--- day122/classwork/cw02.js
+++ day122/classwork/cw02.js
@@ -8,8 +8,8 @@ let func2 = new Func("კატა", 3)
 let func3 = new Func("დათვი", 6)
 let func4 = new Func("ლომი", 7)
 
-
 console.log(func1)
 console.log(func2)
 console.log(func3)
 console.log(func4)
+
