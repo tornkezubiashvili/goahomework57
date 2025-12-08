ab17d7973fe31d04bcab8b7a15f1747ce0801918
tornikezubiashvili <gtornike64@gmail.com>
2025-12-08 22:32:24 +0400

    commit

--- day123/classwork/cw02.js
+++ day123/classwork/cw02.js
@@ -1,19 +1,19 @@
 let form = document.getElementById("form")
 
-function User(name, nge) {
-    this.Name = Name
-    this.Age = Age
+function User(name, age) {
+    this.name = name
+    this.nge = age
     this.user1 = function func() {
        return `my name is ${this.name} my age is ${this.age}`
     }
 
 }
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", function(e) {
     e.preventDefault()
     let Usr = new User(e.target.name.value, e.target.age.value)
     let p = document.createElement("p")
-    p.textContent = Usr.user1
+    p.textContent = Usr.user1()
     body.appendChaild(myP)
     
 })
