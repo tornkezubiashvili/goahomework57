cc181bcbde9de3a92aec97c88dc17ebe90aceedc
tornikezubiashvili <gtornike64@gmail.com>
2025-12-24 11:47:23 +0400

    commit

---
+++ day127/homework/hw01.js
@@ -0,0 +1,72 @@
+let days = document.getElementById("Days")
+let hours = document.getElementById("Hours")
+let mins = document.getElementById("Mins")
+let seconds = document.getElementById("Seconds")
+let form = document.getElementById("form")
+
+let newYear = new Date("2026-1-1")
+
+function time() {
+    let current = new Date()
+    let a = newYear - current - 14800000
+    if (a <= 0) {
+        days.textContent = 0
+        hours.textContent = 0
+        mins.textContent = 0
+        seconds.textContent = 0
+    }
+
+    let Days = Math.floor(a / (1000 * 60 * 60 * 24))
+    let Seconds = Math.floor(a % (1000 * 60) / 1000)
+    let Mins = Math.floor(a % (1000 * 60 * 60) / (1000 * 60))
+    let Hours = Math.floor(a % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    if(Days< 10){
+        days.textContent = `0 ${Days}`
+    }else{
+        days.textContent = Days
+    }
+
+
+    if(Hours< 10){
+        hours.textContent = `0 ${Hours}`
+    }else{
+        hours.textContent = Hours
+    }
+
+
+    if(Mins< 10){
+        mins.textContent = `0 ${Mins}`
+    }else{
+        mins.textContent = Mins
+    }
+
+
+    if(Seconds< 10){
+        seconds.textContent = `0 ${Seconds}`
+    }else{
+        seconds.textContent = Seconds
+    }
+
+   
+    
+   
+}
+
+setInterval(time, 1000)
+let current = new Date()
+let a = newYear - current
+
+if (a <= 0) {
+    let h2 = document.createElement('h3')
+    h2.textContent = "HAPPY NEW YEAR!"
+    document.body.appendChild(h3)
+}
+
+form.addEventListener("change", function (e) {
+    let time = new Date(e.target.value)
+    if (time < current) {
+        alert("Enter the future")
+    } else {
+        newYear = time
+    }
+})
