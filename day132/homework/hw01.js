c1e08ab0e2e3afcb230034db18f3b3a0b9229424
tornikezubiashvili <gtornike64@gmail.com>
2026-01-21 14:56:47 +0400

    commit

---
+++ day132/homework/hw01.js
@@ -0,0 +1,11 @@
+// კომენტარებით ახსენით, რა არის scope JavaScript-ში, მოიყვანეთ მაგალითები
+
+// JavaScript-ში scope ნიშნავს,  რა ნაწილშია ხელმისაწვდომი ცვლადი ან ფუნქცია კოდის ფარგლებში. 
+
+let message = "Hello World"
+
+function showMessage() {
+    console.log(message)
+}
+
+showMessage()
---
+++ day132/homework/hw02.js
@@ -0,0 +1,6 @@
+// კომენტარებით ახსენით, რა არის hoisting-ი
+
+console.log(a) 
+// undefined (არ აგდებს ერორს)
+
+var a = 24
