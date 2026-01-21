64a97354c8e071f42104f4d5eb64bb638febec21
tornikezubiashvili <gtornike64@gmail.com>
2026-01-21 16:49:27 +0400

    commit

--- day132/homework/product-list-with-cart-main/product-list-with-cart-main/script.js
+++ day132/homework/product-list-with-cart-main/product-list-with-cart-main/script.js
@@ -1,5 +1,41 @@
-let add = document.getElementsByClassName("add")
+let addbutton = document.getElementsByClassName("add")
+let quantity = document.getElementsByClassName("quantity")
 
-let plius = document.getElementById("plus")
-let minus = document.getElementById("minus")
+
+let minuss = document.getElementsByClassName("minuss")
+let plusss = document.getElementsByClassName("plusss")
+let p = document.getElementsByClassName("p")
+
+let addeditems = document.getElementById("addeditems")
+
+
+let Pquantity = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+let a = 0
+
+for (let i = 0; i <= addbutton.length; i++) {
+    addbutton[i].addEventListener("click", function () {
+        addbutton[i].style.display = "none"
+        quantity[i].style.display = "block"
+        quantity[i].style.display = "flex"
+
+
+        plusss[i].addEventListener("click", function () {
+            Pquantity[i] += 1
+            p[i].textContent = Pquantity[i]
+
+        })
+
+        minuss[i].addEventListener("click", function () {
+            if (Pquantity[i] !== 0) {
+                Pquantity[i] -= 1
+                p[i].textContent = Pquantity[i]
+            }
+        })
+    })
+    quantity[i].addEventListener("click", function () {
+        addeditems.style.display = "none"
+    })
+}
+
+
 
