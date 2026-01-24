9d57c32255c7639e6d10cbbbfb191c8484679331
tornikezubiashvili <gtornike64@gmail.com>
2026-01-25 00:02:37 +0400

    commit

---
+++ day133/homework/hw07.js
@@ -0,0 +1,12 @@
+//7)დაწერე კოდი, სადაც არის:ერთი global ცვლადი ორი
+// block scope ცვლადი (ერთი if ბლოკში და მეორე for ციკლში)შენი დავალებაა console.log–ით გამოიტანე ყველა ცვლადი ბლოკის გარეთ და ნახე შედეგი
+
+let namee = "tornike"
+
+if(namee === "tornike"){
+    let surname = "zubiashvili"
+    let addres = "gori"
+}
+
+
+console.log(namee + surname + addres)
