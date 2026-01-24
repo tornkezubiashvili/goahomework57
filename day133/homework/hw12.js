d702e6d8b65e57da84bf8aaa95b956da8cc2b912
tornikezubiashvili <gtornike64@gmail.com>
2026-01-25 00:15:04 +0400

    commit

---
+++ day133/homework/hw12.js
@@ -0,0 +1,13 @@
+//12)
+let name = "Goga";
+
+function changeName() {
+  name = "Nika";
+}
+
+changeName();
+console.log(name);
+
+//რა გამოვა და რატომ?
+
+// გამოვიდა ნიკა რადგან ჩვენ ფუნქციის დროს ცვლას უბრალოც ვცვლით
---
+++ day133/homework/hw14.js
@@ -0,0 +1,16 @@
+//14)დაწერე კოდი, სადაც:
+
+//შექმენი global ცვლადი name
+
+//if ბლოკში შექმენი ცვლადი name და დააწერე სხვა მნიშვნელობა
+
+//დააბეჭდე name ბლოკის გარეთ
+
+let name = "tornike"
+
+
+if(true){
+    let name = "saba"
+}
+
+console.log(name)
