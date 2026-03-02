8e2504cc776fdd6509ef1e76d95b4f08b6fe1ea3
tornikezubiashvili <gtornike64@gmail.com>
2026-03-02 22:44:09 +0400

    commit

---
+++ day144/classwork/cw02.js
@@ -0,0 +1,36 @@
+// 2) შექმენით სია რომელშიც შეინახავთ რამდენიმე ობიექტს, თითოეული ობიექტი იქნება განსხვავებული, თქვენი მიზანია მოახდინოთ სიის დესტრუქცია შემდეგ კი თითოეული ობიექტის დესტრუქცია და მათი მნიშვნელობების დაბეჭდვა
+
+let objectarray = [
+    Object1 = {
+        name1 : "tornike",
+        surname : "zubiashvili",
+        age : 17
+    },
+    Object2 = {
+        brand : "mersedes",
+        model : "CLS 63S",
+        year : 2014
+    },
+    Object3 = {
+        fruit1 : "მსხალი",
+        fruit2 : "ვაშლი",
+        fruit3 : "ატამი",
+
+    }
+]
+let [Obj1,Obj2,Obj3] = objectarray
+
+let {name1 , surname, age} = Obj1
+console.log(name1)
+console.log(surname)
+console.log(age)
+
+let {brand , model, year} = Obj2
+console.log(brand)
+console.log(model)
+console.log(year)
+
+let {fruit1 , fruit2, fruit3} = Obj3
+console.log(fruit1)
+console.log(fruit2)
+console.log(fruit3)
