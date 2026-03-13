c5a97294310a8e323ae8495a3b009e6655a367f6
tornikezubiashvili <gtornike64@gmail.com>
2026-03-13 21:18:14 +0400

    commit

--- day146/homework/hw02.js
+++ day146/homework/hw02.js
@@ -1,9 +1,9 @@
 // 2) გამოიყენე forEach() და იპოვე ყველა რიცხვის ჯამი. const numbers = [10, 20, 30, 40];
 
 const numbers = [10, 20, 30, 40]
-let Total = 0
+let sum = 0
 numbers.forEach((num) => {
-    Total += num
+    sum += num
 })
 
-console.log(Total)
+console.log(sum)
