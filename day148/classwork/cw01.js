648d2e504bc9619082af619a8c28148ba2fa41e3
tornikezubiashvili <gtornike64@gmail.com>
2026-03-16 21:40:00 +0400

    commit

---
+++ day148/classwork/cw01.js
@@ -0,0 +1,19 @@
+// 1)შექმენი სია სადაც მოთავსებული იქნება ადამიანის სახელები , დაახლოებით 10 სახელი
+
+// თქბენიდ დავალებაა რომ filter მეთოდის დახმარებით შექმნათ ახალი სია სადაც ინქებიან მხოლოდ ის სახელები რომელშიც ასობის რაოდენობა იქნება 5 ზე მეტი და და იწყება g ასოზე
+
+// ეს დავალება შეასრულეთ single line function ითაც და ჩვეულებრივ return keyword ის გამოყენებითაც
+
+
+
+let names = ["tornike","gggggoga","nika","diana","luka","gabrieli","elene","qetevani","gio","andria"]
+
+let FilterName = names.filter(name => name.length > 5 && name[0] === "g") 
+
+console.log(FilterName)
+
+
+let FilterName2 = names.filter(name2 =>{
+    return  name2.length > 5 && name2[0] === "g"
+})
+console.log(FilterName2)
