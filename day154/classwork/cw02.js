66b7db61e6027930aa13b9bce8a70c6ab3caab74
tornikezubiashvili <gtornike64@gmail.com>
2026-04-09 20:57:15 +0400

    commit

---
+++ day154/classwork/cw02.js
@@ -0,0 +1,23 @@
+
+
+// შ
+// ექმენი Map, სადაც:
+
+// key = ელემენტი
+// value = რამდენჯერ გვხვდება
+
+// საბოლოოდ უნდა მიიღო მსგავსი:
+
+let arr = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
+
+let map = new Map()
+
+for(let i of arr){
+    if(!map.has(i)){
+        map.set(i,1)
+    }
+    if(map.has(i)){
+        map.set(i,)
+    }
+}
