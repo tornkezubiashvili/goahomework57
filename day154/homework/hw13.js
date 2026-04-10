97b9777092f459c89593a452494050b6a9ed136b
tornikezubiashvili <gtornike64@gmail.com>
2026-04-10 15:05:23 +0400

    commit

--- day154/homework/hw13.js
+++ day154/homework/hw13.js
@@ -11,16 +11,26 @@
 let numbers = [1,2,3,4,5,6,7,8,9];
 
 
-let map = new Map([])
+let map = new Map([
+    ["even",0],
+    ["odd",0]
+])
 
+let ev = []
+let od = []
 
 for(let i of numbers){
     if(i %2 === 0){
-        map.set("even", + i)
+       ev.push(i)
     }else{
-        map.set("odd", +i)
+        od.push(i)
     }
 }
+
+map.set("even",ev)
+map.set("odd",od)
+
+
 
 
 console.log(map)
