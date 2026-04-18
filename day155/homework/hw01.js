c8ea0c38f6476fda3e715b2305791064e8a44444
tornikezubiashvili <gtornike64@gmail.com>
2026-04-18 15:35:19 +0400

    commit

---
+++ day155/homework/hw01.js
@@ -0,0 +1,66 @@
+// დავალება: "ალქიმიკოსის ლაბორატორია და გილდიის საცავი"
+// შესავალი: შენი მიზანია შექმნა ლოგიკა, რომელიც მართავს მოთამაშეების ინვენტარს, ამოწმებს აქვთ თუ არა საკმარისი რესურსები რთული ელექსირების დასამზადებლად და ფილტრავს გამოუსადეგარ ნივთებს.
+
+// ნაბიჯი 1: გილდიის საცავი (Union - გაერთიანება)
+// შექმენი ორი Set: player1Bag და player2Bag (მაგალითად, შიგნით ჩაყარე: "Bloodthorn", "Water", "Glowspore", შეურიე საერთო და უნიკალური ნივთები).
+
+// დაწერე ფუნქცია createGuildStash(bag1, bag2), რომელიც დააბრუნებს ახალ Set-ს — ორივე მოთამაშის ნივთების საერთო საცავს (დუბლიკატების გარეშე).
+
+// ნაბიჯი 2: რეცეპტის შემოწმება (Subset / Superset)
+// შექმენი Set სახელად healthPotionRecipe (მაგალითად, სჭირდება: "Water", "Bloodthorn", "Crystal").
+
+// დაწერე ფუნქცია canCraft(recipe, stash).
+
+// ფუნქციამ უნდა შეამოწმოს, მოიპოვება თუ არა რეცეპტის ყველა ინგრედიენტი გილდიის საცავში (ანუ არის თუ არა რეცეპტი საცავის ქვე-სიმრავლე). თუ მოიპოვება — დააბრუნოს true, წინააღმდეგ შემთხვევაში — false.
+
+// ნაბიჯი 3: რა გვაკლია? (Advanced Difference)
+// თუ canCraft აბრუნებს false-ს, მოთამაშემ ზუსტად უნდა იცოდეს რა აკლია, რათა მოსაძებნად წავიდეს.
+
+// დაწერე ფუნქცია getMissingIngredients(recipe, stash), რომელიც დააბრუნებს ახალ Set-ს მხოლოდ იმ ინგრედიენტებით, რომლებიც რეცეპტშია მოთხოვნილი, მაგრამ საცავში არ გვაქვს.
+
+// ნაბიჯი 4: ექსკლუზიური ვაჭრობა (Symmetric Difference)
+// წარმოიდგინე, რომ მოთამაშეებს ერთმანეთში ნივთების გაცვლა უნდათ, მაგრამ აინტერესებთ მხოლოდ ისეთი ნივთები, რაც ერთს აქვს და მეორეს არა.
+
+// დაწერე ფუნქცია getTradeableItems(bag1, bag2), რომელიც დააბრუნებს ისეთ Set-ს, სადაც მოხვდება ნივთები პირველი ჩანთიდან (რაც მეორეში არაა) პლუს ნივთები მეორე ჩანთიდან (რაც პირველში არაა).
+
+// ნაბიჯი 5: ნაგვის გაფილტვრა (Array filtering with Set)
+// მოთამაშემ ტყეში იპოვა ბევრი ნივთი, თუმცა ზოგიერთი უსარგებლოა:
+// const gatheredLoot = ["Wood", "Rotten Flesh", "Bloodthorn", "Mud", "Wood", "Crystal", "Mud"];
+
+// შექმენი Set სახელად trashItems (შიგნით ჩაწერე "Rotten Flesh" და "Mud").
+
+// დაწერე ფუნქცია cleanLoot(lootArray, trashSet), რომელიც გადაურბენს მოპოვებული ნივთების მასივს, ამოაგდებს ნაგავს (გამოიყენე Set.has() ოპერაციის სისწრაფისთვის) და დააბრუნებს მხოლოდ სუფთა, უნიკალური სასარგებლო ნივთების Set-ს.
+
+
+
+let player1Bag = new Set(["Bloodthorn", "Water", "Glowspore", "Stormbark","Sparkthorn","Poison"])
+let player2Bag = new Set(["Bloodthorn", "Water","Thunderseed","Dark","Sparkthorn","Rustbloom"])
+
+let NewBag = new Set()
+
+function createGuildStash(bag1, bag2){
+    for(let i of bag1){
+        NewBag.add(i)
+    }
+    for(let i of bag2){
+        NewBag.add(i)
+    }
+}
+createGuildStash(player1Bag, player2Bag)
+
+let healthPotionRecipe = new Set(["Water", "Bloodthorn", "Crystal"])
+
+let bolean = true
+
+function canCraft(recipe, stash){
+    recipe.forEach(items => {
+        stash.has(items)? bolean = true: bolean = false
+        if(bolean == false){
+            return
+        }
+    })
+    console.log(bolean)
+    
+}
+
+canCraft(healthPotionRecipe, NewBag)
