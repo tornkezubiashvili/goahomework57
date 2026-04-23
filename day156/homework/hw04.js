b8021952fb47b0e186da283f4ca61ed4935915fe
tornikezubiashvili <gtornike64@gmail.com>
2026-04-23 18:31:48 +0400

    commit

--- day156/homework/hw04.js
+++ day156/homework/hw04.js
@@ -6,25 +6,51 @@
 // შეინახე localStorage-ში
 // refresh-ზე ყველა task დაბრუნდეს
 
+// წინა დავალებას დაამატე:
+
+// დავალება:
+
+// თითო task-ს ჰქონდეს delete ღილაკი
+// წაშლისას localStorage-იც განახლდეს
+
 let input = document.getElementById("input")
 let button = document.getElementById("button")
 let div = document.getElementById("div")
 
-let list = []
-let li = document.createElement("li")
+let list = JSON.parse(localStorage.getItem("lst")) || []
+let name = "giorgi" || []
 
-for(let i of list){
-    li.textContent = i
+function rander() {
+    div.innerHTML = ""
+    list.forEach(element => {
+        let li = document.createElement("li")
+        let btn = document.createElement("button")
+        btn.textContent = "delete"
+        btn.classList = "btndelete"
+        li.textContent = element
+        div.appendChild(li)
+        li.appendChild(btn)
+
+
+    });
 }
-button.addEventListener("click", function () {
 
-    li.textContent = input.value
-    div.appendChild(li)
-    list.push(li.textContent)
-
-    localStorage.setItem("lst", list)
-    console.log(localStorage.getItem("lst"))
-
-
-
+div.addEventListener("click", function (e) {
+    if (e.target.classList.contains("btndelete")) {
+        let li = e.target.parentElement
+        let index = Array.from(div.children).indexOf(li)
+        list.splice(index,1)
+        localStorage.setItem("lst",JSON.stringify(list))
+        rander()
+    }
 })
+
+button.addEventListener("click", function () {
+    let user = input.value
+    list.push(user)
+    localStorage.setItem("lst", JSON.stringify(list))
+    rander()
+})
+rander()
+
+
