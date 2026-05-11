9df0c1cef56aa067ce2050623db1a59de4f38dd0
tornikezubiashvili <gtornike64@gmail.com>
2026-05-11 22:58:21 +0400

    commit

--- day159/homework/hw01.js
+++ day159/homework/hw01.js
@@ -1,37 +1,97 @@
 let form = document.getElementById("form")
 let AddDiv = document.getElementById("AddDiv")
+let left = document.getElementById("left")
+let right = document.getElementById("right")
+let DeleteOll = document.getElementById("DeleteOll")
+
 let array = JSON.parse(localStorage.getItem("LocalInput")) || []
+
+right.textContent =  array.length
 
 
 function createElement(text) {
 
     let InpDiv = document.createElement("div")
-    AddDiv.appendChild(InpDiv)
+    InpDiv.className = "InpDiv"
+
+
     let TextDiv = document.createElement("div")
-    InpDiv.appendChild(TextDiv)
+    TextDiv.className = "TextDiv"
     let SirclDiv = document.createElement("div")
+    SirclDiv.className = "SirclDiv"
     TextDiv.appendChild(SirclDiv)
+
+    
     let h3 = document.createElement("h3")
     h3.textContent = text
-    TextDiv.appendChild(h3)
-    array.push(h3.textContent)
+    h3.className = "h3text"
     h3.style.color = "white"
+    TextDiv.appendChild(h3)
+
+    InpDiv.appendChild(TextDiv)
+
+
 
     let imgdiv = document.createElement("div")
+    imgdiv.className = "imgdiv"
 
-    InpDiv.appendChild(imgdiv)
     let editimg = document.createElement("img")
     editimg.src = "img/edit.png"
+    editimg .className = "editimg"
     let deleteimg = document.createElement("img")
-    deleteim.src = "img/delete.png"
+    deleteimg.src = "img/delete.png"
+    deleteimg .className = "deleteimg"
+
+    deleteimg.addEventListener("click",function(){
+        InpDiv.remove()
+        array = array.filter((elemnt)=>{
+          return  elemnt !== h3.textContent 
+
+    
+        })
+
+        localStorage.setItem("LocalInput", JSON.stringify(array))
+        right.textContent =  array.length
+        
+    })
+
     imgdiv.appendChild(editimg)
     imgdiv.appendChild(deleteimg)
 
+    InpDiv.appendChild(imgdiv)
+
+    AddDiv.appendChild(InpDiv)
+
+
+
+
 }
+
 
 form.addEventListener("submit", function (e) {
     e.preventDefault()
-    localStorage.setItem("LocalInput", JSON.stringify(array))
     let InputValue = e.target.input.value
-    createElement(InputValue)
+    if(InputValue.length === 0 || array.includes(InputValue)){
+        return
+    }
+    array.push(InputValue)
+    localStorage.setItem("LocalInput", JSON.stringify(array))
+
+    createElement(InputValue)   
+    right.textContent =  array.length
+
+    form.reset()
+})
+
+
+
+
+for(let i in array){
+    createElement(array[i])
+}
+
+DeleteOll.addEventListener("click",function(){
+    array = []
+    localStorage.setItem("LocalInput", JSON.stringify(array))
+    AddDiv.innerHTML = ""
 })
