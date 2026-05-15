5bc536e81e835a0876e6e55658947b21d983c0a2
tornikezubiashvili <gtornike64@gmail.com>
2026-05-15 23:09:01 +0400

    commit

--- day160/homework/hw01.js
+++ day160/homework/hw01.js
@@ -3,14 +3,32 @@ let AddDiv = document.getElementById("AddDiv")
 let left = document.getElementById("left")
 let right = document.getElementById("right")
 let DeleteOll = document.getElementById("DeleteOll")
+let todofilter = document.getElementById("todofilter")
 
 let array = JSON.parse(localStorage.getItem("LocalInput")) || []
+let filter = localStorage.getItem("filter") || "tasks"
 
-right.textContent =  array.length
+right.textContent = array.length
 
+function filterfunc() {
+    AddDiv.innerHTML = ""
+    if (todofilter.value === "completed-task") {
+        for (let i in array) {
+            if (array[i].isComplited === true) {
+                createElement(array[i].value, i)
+            }
+        }
+    } else {
+        for (let i in array) {
+            if (array[i].isComplited === false) {
+                createElement(array[i].value, i)
+            }
+        }
+    }
+}
 
-function createElement(text) {
-
+function createElement(text, index) {
+    index = index - 1
     let InpDiv = document.createElement("div")
     InpDiv.className = "InpDiv"
 
@@ -19,9 +37,19 @@ function createElement(text) {
     TextDiv.className = "TextDiv"
     let SirclDiv = document.createElement("div")
     SirclDiv.className = "SirclDiv"
+    SirclDiv.id = index
     TextDiv.appendChild(SirclDiv)
 
-    
+    SirclDiv.addEventListener("click", function (e) {
+        let itemIndex = e.target.id
+        array[itemIndex].isComplited = true
+        SirclDiv.style.backgroundColor = "red"
+        localStorage.setItem("LocalInput", JSON.stringify(array))
+        filterfunc()
+
+    })
+
+
     let h3 = document.createElement("h3")
     h3.textContent = text
     h3.className = "h3text"
@@ -37,22 +65,22 @@ function createElement(text) {
 
     let editimg = document.createElement("img")
     editimg.src = "img/edit.png"
-    editimg .className = "editimg"
+    editimg.className = "editimg"
     let deleteimg = document.createElement("img")
     deleteimg.src = "img/delete.png"
-    deleteimg .className = "deleteimg"
+    deleteimg.className = "deleteimg"
 
-    deleteimg.addEventListener("click",function(){
+    deleteimg.addEventListener("click", function () {
         InpDiv.remove()
-        array = array.filter((elemnt)=>{
-          return  elemnt !== h3.textContent 
+        array = array.filter((elemnt) => {
+            return elemnt !== h3.textContent
 
-    
+
         })
 
         localStorage.setItem("LocalInput", JSON.stringify(array))
-        right.textContent =  array.length
-        
+        right.textContent = array.length
+
     })
 
     imgdiv.appendChild(editimg)
@@ -71,27 +99,63 @@ function createElement(text) {
 form.addEventListener("submit", function (e) {
     e.preventDefault()
     let InputValue = e.target.input.value
-    if(InputValue.length === 0 || array.includes(InputValue)){
+    if (InputValue.length === 0 || array.includes(InputValue)) {
         return
     }
-    array.push(InputValue)
+    let todoItem = {
+        value: InputValue,
+        isComplited: false
+    }
+    array.push(todoItem)
     localStorage.setItem("LocalInput", JSON.stringify(array))
 
-    createElement(InputValue)   
-    right.textContent =  array.length
+    createElement(todoItem.value, array.length)
+    right.textContent = array.length
 
     form.reset()
 })
 
 
+filterfunc()
 
+// if (filter === "completed-task") {
+//     for (let i in array) {
+//         if (array[i].isComplited === true) {
+//             createElement(array[i].value, i)
+//         }
+//     }
+// } else {
+//     for (let i in array) {
+//         if (array[i].isComplited === false) {
+//             createElement(array[i].value, i)
+//         }
+//     }
+// }
 
-for(let i in array){
-    createElement(array[i])
-}
-
-DeleteOll.addEventListener("click",function(){
+DeleteOll.addEventListener("click", function () {
     array = []
     localStorage.setItem("LocalInput", JSON.stringify(array))
     AddDiv.innerHTML = ""
+
+})
+
+
+
+todofilter.addEventListener("change", function () {
+    console.log(todofilter.value)
+    localStorage.setItem("filter", todofilter.value)
+    filterfunc()
+    // if (todofilter.value === "completed-task") {
+    //     for (let i in array) {
+    //         if (array[i].isComplited === true) {
+    //             createElement(array[i].value, i)
+    //         }
+    //     }
+    // } else {
+    //     for (let i in array) {
+    //         if (array[i].isComplited === false) {
+    //             createElement(array[i].value, i)
+    //         }
+    //     }
+    // }
 })
