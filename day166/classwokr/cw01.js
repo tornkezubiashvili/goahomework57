a75d392a08ee04417d698739a297b506d81a8f51
tornikezubiashvili <gtornike64@gmail.com>
2026-05-31 12:24:57 +0400

    commit

--- day166/classwokr/cw01.js
+++ day166/classwokr/cw01.js
@@ -1,6 +1,25 @@
-
-
-
 function getDataFromBackend(){
-    
+    let http = new XMLHttpRequest()
+
+    http.onreadystatechange = function(){
+        console.log(this.readyState)
+        if(this.readyState == 4 && this.status === 200){
+            
+            let data = this.responseText
+            let parseData =  JSON.parse(data)
+            console.log(parseData)
+            for(let i = 0;i<parseData.length;i++){
+                console.log(parseData[i])
+                let li = document.createElement("li")
+                li.textContent = parseData[i].title
+                document.body.appendChild(li)
+            }
+        }
+
+    }
+
+    http.open("GET","https://jsonplaceholder.typicode.com/todos")
+    http.send()
 }
+
+getDataFromBackend()
