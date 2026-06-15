b2d52308ea0e6fb9ad45648f9aea77a06e6e1a13
tornikezubiashvili <gtornike64@gmail.com>
2026-06-15 22:23:40 +0400

    commit

---
+++ day170/classwokr/cw01.js
@@ -0,0 +1,21 @@
+// 1) შექმენით ფუნქცია რომელიც ასინქრონულად დააბრუნებს მონაცემბს(მონაცემი იქნება ამინდის პროგნოზი), ამ ფუნქციას გადაეცემა callback რომელიც მიიღებს არგუმენტად ამინდის პროგნოზის მონაცემს და გამოსახავს შესაბამის რჩევას
+
+func(weather)
+
+function weather(obj) {
+    if (obj === "Sunny") {
+        console.log("მზიანი ამინდია")
+    }
+    else if (obj === "Clody") {
+        console.log("ღრუბლიანი ამინდია")
+    }
+    else if (obj === "Rain") {
+        console.log("წვიმიანი ამინდია")
+    }
+}
+
+function func(callback){
+    setTimeout(() => {
+        callback("Clody")
+    })
+}
