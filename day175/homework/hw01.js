22b7af93511c1e9e95a018764e5d5b1e0dc14dbf
tornikezubiashvili <gtornike64@gmail.com>
2026-07-06 14:34:58 +0400

    commit

---
+++ day175/homework/hw01.js
@@ -0,0 +1,27 @@
+// ნებისმიერი api ის გამოყენებით ააწყვეთ მინი პროექტი სადა გამოიყენებთ async await და try catch ბლოკებს
+
+
+let form = document.getElementById("form")
+let p = document.getElementById("p")
+
+
+
+try{
+    async function api() {
+        let response = await fetch("https://v6.exchangerate-api.com/v6/5ff86e573d0e12ed27d616d6/latest/USD")
+        let data = await response.json()
+        form.addEventListener("submit",function(e){
+            e.preventDefault()
+            let input = e.target.search.value
+            let text = input.toUpperCase()
+            let apiText = data.conversion_rates[text]
+            
+            p.textContent = `${text} : ${apiText}`
+        })
+        
+    }
+    api()
+
+}catch(error){
+    console.log(error)
+}
