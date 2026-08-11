4ee0b6f4a4f2fa8b75698cb6b88ab552397ef32a
tornikezubiashvili <gtornike64@gmail.com>
2026-08-11 21:38:32 +0400

    commit

---
+++ day183/classwork/reactFolder2/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+    return (
+        <div>
+            <p>heloooooooooooooooo</p>
+            <iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1051045324229341%2F&show_text=false&width=380&t=0" width="380" height="476" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" ></iframe>
+
+            <iframe width="1337" height="752" src="https://www.youtube.com/embed/0UkRNOZKrqk" title="ჩინეთმა პრადო მოკლა? | Jetour G700 904 ცხენის ძალიანი Prado!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+            <iframe width="1337" height="752" src="https://www.youtube.com/embed/qwk7fbuvKbI" title="კინოცოდვა -  ფორსაჟი 9 / Fast saga 9 (2021)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+            <img src="https://scontent.ftbs9-1.fna.fbcdn.net/v/t39.99422-6/763003862_27631410699861308_4882583441601901545_n.png?stp=dst-jpg_tt6&cstp=mx1538x2048&ctp=s590x590&_nc_cat=102&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHyQHjZJqDesV7N6jOQr1hqNna4E1avyas2drgTVq_JqwDECoJHegb9Y-waBf7kFBU-b3-r1FN1hb3KYQOjCFOB&_nc_ohc=xo--G6VDwyYQ7kNvwGrGDmb&_nc_oc=Adrjh68jMHEued8VRijTvthGxs0n1sAmquhCDI2Eyg2u1TQD6gS1PXVO3X_5TlgWN8M&_nc_zt=14&_nc_ht=scontent.ftbs9-1.fna&_nc_gid=6D0lnqCBg1FBEF6sA-RmvQ&_nc_ss=7b2a8&oh=00_AQHuUVf65OZBf78nQ9wjg-ie9c8DBMHcqIYtW4Fp5c6ZwA&oe=6A7FF8AB" alt="" />
+        </div>
+    )
+
+}
+
+export default App
---
+++ day183/classwork/reactFolder2/src/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <nav>NAV</nav>
+        </header>
+    )
+}
+
+export default Header
