0454914db76e8b01722cddebd0b7ab4e1de7f061
tornikezubiashvili <gtornike64@gmail.com>
2026-08-17 22:55:36 +0400

    commit

---
+++ day185/classwokr/reactproject/src/App.jsx
@@ -0,0 +1,20 @@
+import Car from './card.jsx'
+
+function App() {
+
+    return (
+        <div>
+            <Car imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8wF1xqMGLuZDQp79mGXrJJUHahUyQ4HJZPaIG-fiRLf61NFREXNPyyV8&s=10" desc="jetta 2.5" />
+
+            <Car imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj9BA3m8hT2g1AdxQfl4PWwMYBaxRw7K0xQUs6XUyDv_iX_j6dJ6sMW6U&s=10" desc="W211 E55 AMG" />
+
+            <Car imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRimQ6iDYLAfR1mD_k-Va7q8FqejSnHCBQOMVrr0EnN_Tnr7ypHL4wePBnv&s=10" desc="CLS63S AMG" />
+
+        </div>
+
+
+    )
+
+}
+
+export default App
---
+++ day185/classwokr/reactproject/src/card.jsx
@@ -0,0 +1,14 @@
+
+
+function Car(props){
+    return(
+        <div>
+            <p>{props.desc}</p>
+            <img src={props.imgsrc} alt="" />
+            
+        </div>
+    )
+}
+
+
+export default Car
