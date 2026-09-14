a7bf14d6b2c7b3afd6ed10b9ba39bae87b3c4fb9
tornikezubiashvili <gtornike64@gmail.com>
2026-09-14 20:15:43 +0400

    commit

--- day191/homework/reactcss/src/hw06.jsx
+++ day191/homework/reactcss/src/hw06.jsx
@@ -1,18 +1,37 @@
 function Hw06() {
-    const users = [
-        {
-            id: 1,
-            name: "Giorgi",
-            age: 22,
-            skills: ["JavaScript", "React", "CSS"]
-        },
-        {
-            id: 2,
-            name: "Nika",
-            age: 25,
-            skills: ["Python", "Django"]
-        }
-    ];
+  const users = [
+    {
+      id: 1,
+      name: "Giorgi",
+      age: 22,
+      skills: ["JavaScript", "React", "CSS"]
+    },
+    {
+      id: 2,
+      name: "Nika",
+      age: 25,
+      skills: ["Python", "Django"]
+    }
+  ];
+
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>Age: {user.age}</p>
+
+          <h3>Skills:</h3>
+
+          <ul>
+            {user.skills.map((skill) => (
+              <li key={skill}>{skill}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Hw06
